405196b2f323abdeb86022416b083d2525f6f79f
Beast Industries <null>
2019-01-24 21:54:08 -0500

    Test Base

--- RAGECore/Core.cs
+++ RAGECore/Core.cs
@@ -1,9 +1,71 @@
-﻿using System;
+﻿using GTANetworkAPI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 namespace RAGECore
 {
-    public class Core
+    public interface RAGEModule
     {
+        int Version { get; set; }
+        string Name { get; set; }
+        void Load();
+        void Unload();
+    }
 
+    public class Core : Script
+    {
+        List<object> LoadedModules = new List<object>();
+
+        [ServerEvent(Event.ResourceStart)]
+        public void OnResourceStart()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("-<RAGE Modular Core>- is starting...");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Loading modules...");
+
+            /*TODO: SUPPORT FOR DLL DE-LOADING (CUSTOM APPDOMAIN)*/
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            foreach(string dll in Directory.GetFiles(path + "\\Modules\\", "*.dll"))
+            {
+                Assembly ass = Assembly.LoadFile(dll);
+                Type type = ass.GetExportedTypes()[0]; //check later for epic fail.....
+                if(!typeof(RAGEModule).IsAssignableFrom(type)) //epic fail....
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error Loading Module -<{type.ToString()}>-: Missing RAGEModule Interface");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"Loading Module -<{type.ToString()}>-...");
+                }
+                try
+                {
+                    object c = Activator.CreateInstance(type);
+                    LoadedModules.Add(c);
+
+                    Console.ForegroundColor = ConsoleColor.White; //in case noob devs forget to switch color in their loads...
+                    (c as RAGEModule).Load();
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Success!");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                catch(Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error Loading Module -<{type.ToString()}>-: {e.ToString()}");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("-<RAGE Modular Core>- has loaded!");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
     }
 }
---
+++ RAGEDatabase/DatabaseManager.cs
@@ -0,0 +1,22 @@
+﻿using GTANetworkAPI;
+using RAGECore;
+using System;
+
+namespace RAGEDatabase
+{
+    public class DatabaseManager : Script, RAGEModule
+    {
+        public int Version { get; set; } = 1;
+        public string Name { get; set; } = "Database Module";
+
+        public void Load()
+        {
+            Console.WriteLine("Hello from database module.... epic victory royale...");
+        }
+        
+        public void Unload()
+        {
+
+        }
+    }
+}
